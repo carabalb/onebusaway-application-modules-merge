80b2233387e58347b2490c4e85c55424f9fc6153
sheldonabrown <sheldonb@gmail.com>
2014-04-14 16:28:35 -0400

    changes to support spring security upgrade
    Former-commit-id: e70333f338cc8af2f5e22922f44416427846648d
    Conflicts:
    	onebusaway-watchdog-webapp/src/main/resources/data-sources.xml

--- onebusaway-transit-data-federation-builder/src/main/java/org/onebusaway/transit_data_federation/bundle/tasks/transit_graph/StopTimeEntriesFactory.java
+++ onebusaway-transit-data-federation-builder/src/main/java/org/onebusaway/transit_data_federation/bundle/tasks/transit_graph/StopTimeEntriesFactory.java
@@ -191,6 +191,8 @@ public class StopTimeEntriesFactory {
             + pindex.distanceFromTarget);
       } catch (DistanceAlongShapeException ex) {
         _invalidStopToShapeMappingExceptionCount++;
+      } catch (IllegalArgumentException iae) {
+        _log.warn("Stop has illegal coordinates along shapes=" + shapePoints);
       }
     }
 
